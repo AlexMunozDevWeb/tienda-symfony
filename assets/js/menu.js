7b30e852a1f3174a58418d93cf98005a6aeaa1aa
alex <alemumu207@gmail.com>
2022-10-18 13:31:18 +0200

    Cerrar sesión

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,17 +1,9 @@
 
 //Abrir carrito
-$( '.cart-icon-container' ).on( 'click', function () {
-  $( '.cart-content' ).toggleClass( 'open' );
+$( '.user-link-photo' ).on( 'click', function () {
+  $( '.close-session' ).toggleClass( 'open' );
 } );
 
-//Cerrar carrito cuando se pincha fuera
-$('body').on('click',function(event){
-  if( $(event.target ).is( '.cart-content' ) || $( event.target ).is( '.cart-icon-container' ) || $( event.target ).is( '.icon-cart' ) 
-      || $( event.target ).is( '.quantity' ) ){
-  }else{
-    $( '.cart-content' ).removeClass( 'open' );
-  }
-});
 
 //Abrir menu
 $( '#open-menu-mobile' ).on( 'click', function () {
