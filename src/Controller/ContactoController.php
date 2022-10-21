00c4f1a3b553870201abdf44103ad6d262931e1e
alex <alemumu207@gmail.com>
2022-10-21 20:02:16 +0200

    Funcionalidad mandar mensaje

--- src/Controller/ContactoController.php
+++ src/Controller/ContactoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Contactanos;
 use App\Entity\Productos;
 use App\Entity\Usuarios;
 use Doctrine\ORM\EntityManagerInterface;
@@ -24,14 +25,25 @@ class ContactoController extends AbstractController
   #[Route('/contacto', name: 'app_contacto')]
   public function index( SessionInterface $session ): Response
   {
-
     $session_started = $this->em->getRepository( Usuarios::class )->checkSessionStart( $session );
     $cart_detail = $this->em->getRepository( Productos::class )->getCart( $session );
-
     return $this->render('contacto/index.html.twig', [
       'session_started' => $session_started,
       'cart_empty' => $session->has('carrito'),
       'quantity_products' => $session->has( 'carrito' ) ? count($cart_detail) : '',
     ]);
+  }
+
+  #[Route('/contacto/enviar', name: 'app_send_msn')]
+  public function send(): Response
+  {
+    //Formulario mandar mensaje
+    if ( isset( $_POST[ 'nombre' ] ) && isset( $_POST[ 'correo' ] ) && isset( $_POST[ 'msn-content' ] ) ) {
+      $name = $_POST[ 'nombre' ];
+      $email = $_POST[ 'correo' ];
+      $content_msn = $_POST[ 'msn-content' ];
+      $this->em->getRepository( Contactanos::class )->addMessage( $name, $email, $content_msn, $this->em );
+    }
+    return $this->redirectToRoute('app_homepage');
   }
 }
