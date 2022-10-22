85160ce902fd3979fdcc8aff72809e90626c853d
alex <alemumu207@gmail.com>
2022-10-22 17:45:11 +0200

    Fix jquery file load in home-page

--- assets/app.js
+++ assets/app.js
@@ -13,7 +13,6 @@ import './styles/app.less';
 import './js/menu.js';
 import './js/homepage';
 import './js/productpage.js';
-import './js/moving-text.js';
 
 // start the Stimulus application
 import './bootstrap';
